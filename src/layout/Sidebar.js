be05bc7e629460d2f85a6f1d6e63e1fd306ea938
van.vuhuu <van.vuhuu@euroland.com>
2021-09-15 23:20:38 +0700

    update sidebar

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -28,11 +28,11 @@ export default class Sidebar extends React.Component{
                 </Link>
                 <div className="sidebar">
                     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                        <div className="image">
+                        {/* <div className="image">
                             <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image"/>
-                        </div>
+                        </div> */}
                         <div className="info">
-                            <a href="#" className="d-block">Admin Shoprunner</a>
+                            <a href="#" className="d-block">Shoprunner Admin page</a>
                         </div>
                     </div>
                     <div className="form-inline">
